1d7b88161aa29e871b3821f999392b7432a798e3
jfnieves <>
2011-05-21 02:05:02 +0000

    Added queue state.

--- filters/inventory/tcl/nbspstatcounters.tcl
+++ filters/inventory/tcl/nbspstatcounters.tcl
@@ -2,11 +2,12 @@
 #
 # $Id$
 #
-# Usage: nbspstatcounters [-f <fmt>] [<statusfile>]
+# Usage: nbspstatcounters [-f <fmt>] [-q <qstatefile>] [-s <statusfile>]
 #
 # nbsp counters in the last minute (ending at "unix_seconds").
 #
 # If no <statusfile> is given the default nbspd.status file is used.
+# If no <qstatefile> is given the default nbspd.qstate file is used.
 # The last line is written to stdout; the order of the columns is given below.
 # The <fmt> can be: std (default), xml, csv, csvk
 # The motivation for the existence of this tool is to use it for extracting
@@ -14,8 +15,8 @@
 # by the _inbsp extension in the web interface).
 
 package require cmdline;
-set usage {nbspstatcounters [-f <fmt>] [<statusfile>]};
-set optlist {{f.arg "std"}};
+set usage {nbspstatcounters [-f <fmt>] [-q <qstatefile>] [-s <statusfile>]};
+set optlist {{f.arg "std"} {q.arg ""} {s.arg ""}};
 
 ## The common defaults
 set _defaultsfile "/usr/local/etc/nbsp/filters.conf";
@@ -38,15 +39,22 @@ unset nbspd_init_file;
 array set option [::cmdline::getoptions argv $optlist $usage];
 set argc [llength $argv];
 
+# defaults
 set fmt $option(f);
-if {$argc >= 1} {
-    set datafile [lindex $argv 0];
-} else {
-    set datafile $nbspd(statusfile);
+set qstatefile $nbspd(qstatelogfile);
+set statusfile $nbspd(statusfile);
+
+if {$option(q) == 1} {
+    set qstatefile $option(q);
+}
+
+if {$option(s) == 1} {
+    set statusfile $option(s);
 }
 
 #
-# These are the fields of the nbspd.status file.
+# These are the fields of the nbspd.status file, followed by the 
+# nbspd.qstate file.
 #
 set keywords [list \
 unix_seconds \
@@ -65,9 +73,14 @@ products_retransmitted_ignored \
 products_retransmitted_recovered \
 products_rejected \
 products_aborted \
-products_bad];
+products_bad \
+queue_processor \
+queue_filter \
+queue_server];
 
-set values [split [exec tail -n 1 $datafile]];
+set qvalues [split [exec tail -n 1 $qstatefile]];
+set svalues [split [exec tail -n 1 $statusfile]];
+set values [concat $svalues [lrange $qvalues 1 end]];
 
 if {$fmt eq "std" } {    
     puts {#
