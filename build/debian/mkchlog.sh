c225cc4a05bf0c691aa14e679ddfe4e00f990669
jfnieves <>
2011-09-12 02:43:52 +0000

    Revised msgfilter because with the tcl mime-smtp package the sms to mobile devices was not working correctly.

--- build/debian/mkchlog.sh
+++ build/debian/mkchlog.sh
@@ -7,5 +7,5 @@ $name ($version-1) stable; urgency=low
 
   * Initial debian package release
 
- -- Jose F Nieves <nieves@ltp.upr.clu.edu>  `./date-R.sh`
+ -- Jose F Nieves <nieves@ltp.uprrp.edu>  `./date-R.sh`
 EOF
