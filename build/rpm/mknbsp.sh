fc02257240c26a861affe4cc2a87e91a84d79c1a
Jose F Nieves <nieves@1-loop.net>
2011-12-12 23:33:38 -0400

    Revised to use the branch instead of tgz file

--- build/rpm/mknbsp.sh
+++ build/rpm/mknbsp.sh
@@ -2,9 +2,10 @@
 
 branchname=nbsp
 
-tgzfile=${branchname}.tgz
-rm -rf $branchname
-tar -xzf $tgzfile
-cd ${branchname}/build/rpm
+#tgzfile=${branchname}.tgz
+#rm -rf $branchname
+#tar -xzf $tgzfile
 
+cd ${branchname}/build/rpm
+make clean
 make package
