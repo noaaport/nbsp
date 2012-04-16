6b760139d83c91cd3e413dd62c13010c90344bfd
Jose F Nieves <nieves@1-loop.net>
2012-04-15 21:49:33 -0400

    Revised export script

--- build/tools/export.sh
+++ build/tools/export.sh
@@ -1,45 +1,20 @@
 #!/bin/sh
 
-project=nbsp
-#
-host="bzr+ssh://repo.1-loop.net"
-#
-repo="home/repo/bzr/noaaport"
-tag=trunk
-#
-site=${host}/${repo}
-
-# nbsptclhttpd receives special treatment
-tcllibs="tclgrads tclgempak tclmetar tclssh tclupperair"
-srclibs="libconnth libqdb libspoolbdb libtclconf"
-tclhttpd=${project}tclhttpd
-
-exclude="build dev-notes examples"
-tmpdir=tmp
-
 # read name and version
 . ../../VERSION
 
+exclude="build dev-notes examples"
+
+tmpdir=tmp
 rm -r -f $tmpdir
 mkdir $tmpdir
 cd $tmpdir
 
-bzr export ${name}-$version $site/$name/$tag
+bzr export ${name}-${version}
 cd ${name}-${version}
 rm -r $exclude
-for p in $tcllibs
-do
-  bzr export $p $site/$p/$tag
-done
-bzr export tclhttpd $site/$tclhttpd/$tag
 
-cd src
-for p in $srclibs
-do
-  bzr export $p $site/$p/$tag
-done
-
-cd ../.. 
+cd ..
 tar -czf ../${name}-${version}.tgz ${name}-${version}
 
 cd ..
