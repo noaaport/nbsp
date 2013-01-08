ff1784622cf12621ad7b350f6a3179d5f617453f
Jose F Nieves <nieves@volkron.net>
2013-01-08 18:40:15 -0400

    Removed old tclmetar

--- tclmetar/build/debian/date-R.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-# Mimics `date -R` which is the format required in the debian changelog file
-date +"%a, %d %b %Y %T %z"
--- tclmetar/src/pkgIndex.tcl
+++
@@ -1,4 +0,0 @@
-#
-# $Id$
-#
-package ifneeded metar 1.0 [list source [file join $dir metar.tcl]]
