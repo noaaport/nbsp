794a42b23951959e0f2e4f5682e854c73c00878f
nieves <nieves@volkron.net>
2014-07-06 12:16:49 -0400

    Non-operational products in craftinsert are now flaged (and discarded) but are not reported as errors.

--- filters/craftfilter/tcl/craftinsert.tcl
+++ filters/craftfilter/tcl/craftinsert.tcl
@@ -6,6 +6,35 @@
 #
 package require Tclx;	# umask
 
+# defaults
+set craftinsert(confdir) "/usr/local/etc/nbsp";
+set craftinsert(conf) [file join $craftinsert(confdir) "craftinsert.conf"];
+set craftinsert(localconfdirs) [list \
+	 [file join $craftinsert(confdir) "defaults"] \
+	 [file join $craftinsert(confdir) "site"]];
+#
+# These are configurable in the configuration file(s)
+#
+set craftinsert(nbspd_enable) 0;
+set craftinsert(nbspd_spooldir) "/var/noaaport/nbsp/spool";
+set craftinsert(nbspd_infifo) "/var/run/nbsp/infeed.fifo";
+set craftinsert(nbspd_wmoid) "level2";
+set craftinsert(mvtospool) 0;  # move to spool or insert (default is ins)
+set craftinsert(umask) "002";
+#
+set craftinsert(ldm_fext) ".tmp";    # must match what is used in pqact.conf
+set craftinsert(non_operational) {DAN1|DOP1|FOP1|NOP4|ROP4|NOP3};
+
+proc msg {s} {
+
+    global argv0;
+
+    set name [file tail $argv0];
+
+    puts stdout "$name: $s";
+    exec logger -t $name $s;
+}
+ 
 proc err {s} {
 
     global argv0;
@@ -25,8 +54,15 @@ proc proc_nbsp {ppath} {
     set spooldir $craftinsert(nbspd_spooldir);
     set wmoid $craftinsert(nbspd_wmoid);
 
-    # extract info from input name
+    # Extract info from input name
     set input_name [file tail $ppath];
+
+    # Ignore non-operational products
+    if {[regexp $craftinsert(non_operational) $input_name]} {
+	msg "Ignoring non-operational file: $input_name";
+	return;
+    }
+
     if {[regexp {^([A-Z]{4})_(\d{8})_(\d{4})} \
 	     $input_name match STATION ymd hm] == 0} {
 	return -code error "Invalid file name: $input_name";
@@ -74,23 +110,6 @@ proc proc_nbsp {ppath} {
 #
 # main
 #
-set craftinsert(confdir) "/usr/local/etc/nbsp";
-set craftinsert(conf) [file join $craftinsert(confdir) "craftinsert.conf"];
-set craftinsert(localconfdirs) [list \
-	 [file join $craftinsert(confdir) "defaults"] \
-	 [file join $craftinsert(confdir) "site"]];
-#
-# These are configurable in the configuration file(s)
-#
-set craftinsert(nbspd_enable) 0;
-set craftinsert(nbspd_spooldir) "/var/noaaport/nbsp/spool";
-set craftinsert(nbspd_infifo) "/var/run/nbsp/infeed.fifo";
-set craftinsert(nbspd_wmoid) "level2";
-set craftinsert(mvtospool) 0;  # move to spool or insert (default is ins)
-set craftinsert(umask) "002";
-#
-set craftinsert(ldm_fext) ".tmp";    # must match what is used in pqact.conf
-
 # Read optional config file
 if {[file exists $craftinsert(conf)]} {
     source $craftinsert(conf);
