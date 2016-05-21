10ebdcbe9ebad52dc82cd0294b1b8ac8af3851b4
nieves <nieves@volkron.net>
2016-05-20 20:12:41 -0400

    Irevised mk-install weatherwire script

--- dev-notes/weatherwire-conffiles/mk-install.sh
+++ dev-notes/weatherwire-conffiles/mk-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cp functionsstatus.tcl ../tclhttpd/lib/nbsp
-cp index.tml.body ../tclhttpd/htdocs/src/templates/index
-cp header.html leftmenu.html topmenu.html ../tclhttpd/htdocs/src/include
+cp index.tml.body ../../tclhttpd/htdocs/src/templates/index
+cp header.html leftmenu.html topmenu.html ../../tclhttpd/htdocs/src/include
