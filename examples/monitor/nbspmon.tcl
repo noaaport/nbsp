34669fb006d29d09dc8a5be53b91c9be6a71e2a8
Jose F Nieves <nieves@volkron.net>
2017-08-09 14:37:19 -0400

    Added an example script for monitoring a remote nbspd via the web interface. The example is in examples/monitor directory.

---
+++ examples/monitor/nbspmon.tcl
@@ -0,0 +1,45 @@
+#!/usr/bin/tclsh
+#
+# Example for monitoring a remote nbspd server using the web interface
+#
+# The scripts nbspstats and nbspstatcounters installed with the package
+# are suitable for monitoring a local nbspd server.  This script
+# is an example of how to use the web interface for monitoring a remote
+# nbspd server.
+# 
+set nodeslist [list "1.nbsp.inoaaport.net" \
+		   "2.nbsp.inoaaport.net" \
+		   "3.nbsp.inoaaport.net" \
+		  ];
+
+set urltmpl {${node}:8015/_inbsp/stats?format=csvk};
+
+foreach node $nodeslist {
+
+    set total_files 0;
+    set total_bytes 0;
+
+    set url [subst $urltmpl];
+    
+    set output [split [exec curl -s $url] ","];
+
+    foreach entry $output {
+	if {[regexp {chstats_files} $entry]} {
+	    set val [lindex [split $entry "="] 1]
+	    incr total_files $val;
+	}
+
+	if {[regexp {chstats_bytes} $entry]} {
+	    set val [lindex [split $entry "="] 1]
+	    incr total_bytes $val;
+	}
+
+	if {[regexp {chstats_start_time} $entry]} {	             
+	    set chstats_time [lindex [split $entry "="] 1];
+	}
+    }
+
+    append r $node " " $chstats_time " " $total_files " " $total_bytes;
+    puts $r;
+    set r "";
+}
