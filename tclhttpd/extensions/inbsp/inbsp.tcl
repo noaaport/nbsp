67715f5a4ee09a23160deef665e8c6c9c789292e
nieves <nieves@volkron.net>
2017-08-16 19:16:10 -0400

    Simplfied inbsp.tcl

--- tclhttpd/extensions/inbsp/inbsp.tcl
+++ tclhttpd/extensions/inbsp/inbsp.tcl
@@ -10,9 +10,10 @@
 # /_inbsp/stats[?format=std|yaml|xml|csv|csvk]     (status file)
 # /_inbsp/missing[?format=xml|csv]   (missing file: unimplemented)
 # /_inbsp/<xxx>[?format=xml|csv]     (other file: unimplemented)
-# /_inbsp/monitor  (inventory filter file <hh>.stats)
+# /_inbsp/monitor[?format=std|yaml|xml|csv|csvk] (inventory filter <hh>.stats)
 #
-# All data is retrieved via the inventory filter tool nbspstatcounters
+# _inbsp/stats data retrieved via the inventory filter tool nbspstatcounters
+# _inbsp/monitor data retrieved via the inventory filter tool nbspstatmonitor
 #
 Direct_Url /_inbsp _inbsp;
 
@@ -20,8 +21,9 @@ set inbsp(conf) "inbsp.conf";
 set inbsp(confdir) $Config(confdir);
 set inbsp(localconfdirs) $Config(localconfdirs);
 
-# Non-configurable
-set inbsp(data_type) "_inbsp/stats";
+# Non-configurable - must be set in each function
+set inbsp(data_type) "";
+set inbsp(data_tool) "";
 
 # The local overrides
 set _inbspconf [file join $inbsp(confdir) $inbsp(conf)];
@@ -33,6 +35,10 @@ unset _inbspconf;
 proc _inbsp/stats {{format "std"}} {
 
     global inbsp;
+
+    # Must be set in each function
+    set inbsp(data_type) "_inbsp/stats";
+    set inbsp(data_tool) "nbspstatcounters";
 
     # The mechanism used to return content types is to set a global
     # variable with the same name as this function.
@@ -54,12 +60,40 @@ proc _inbsp/stats {{format "std"}} {
     return $r;
 }
 
+proc _inbsp/monitor {{format "csv"}} {
+
+    global inbsp;
+
+    # Must be set in each function
+    set inbsp(data_type) "_inbsp/monitor";
+    set inbsp(data_tool) "nbspstatmonitor";
+    
+    # The mechanism used to return content types is to set a global
+    # variable with the same name as this function.
+    set _inbsp/minitor "text/plain";
+
+    if {$format eq "xml"} {
+	set _inbsp/monitor "text/xml";
+	set r "[inbsp_output_xml_start $inbsp(data_type)]\n";
+    } else {
+	set r "";
+    }
+
+    append r [inbsp_output_stats $format];
+
+    if {$format eq "xml"} {
+	append r "\n[inbsp_output_xml_end $inbsp(data_type)]";
+    }
+
+    return $r;
+}
+
 proc inbsp_output_stats {format} {
 
     global inbsp;
 
     set status [catch {
-	set data [exec nbspstatcounters -f $format];
+	set data [exec $inbsp(data_tool) -f $format];
     } errmsg];
 
     if {$status != 0} {
@@ -67,8 +101,6 @@ proc inbsp_output_stats {format} {
     }
 
     # Prepend the data_type and any other metadata before the data
-    # output by npemwinstatcounters (unless the requested format is "stdh"
-    # which contains the header from nbspstatcounters).
     set r "";
     foreach k [list data_type] {
 	if {$format eq "std"} {
@@ -99,76 +131,4 @@ proc inbsp_output_xml_end {type} {
     set r "</${type}>";
 
     return $r;
-}
-
-proc _inbsp/monitor {} {
-
-    # The mechanism used to return content types is to set a global
-    # variable with the same name as this function.
-    set _inbsp/summary "text/plain";
-    
-    # error codes
-    set err_stats_time 1;
-    set err_chstats_time 2;
-    set err_chstats_data 3;
-
-    set total_files 0;
-    set total_bytes 0;
-
-    set status [catch {
-	set rawoutput [exec nbspstatcounters -f "csvk"];
-    } errmsg];
-
-    if {$status != 0} {
-	return "";
-    }
-
-    set output [split $rawoutput ","];
-
-    foreach entry $output {
-	if {[regexp {chstats_files} $entry]} {
-	    set val [string trim [lindex [split $entry "="] 1]];
-	    incr total_files $val;
-	}
-
-	if {[regexp {chstats_bytes} $entry]} {
-	    set val [string trim [lindex [split $entry "="] 1]];
-	    incr total_bytes $val;
-	}
-
-	if {[regexp {chstats_time} $entry]} {
-	    set chstats_time [string trim [lindex [split $entry "="] 1]];
-	}
-
-	if {[regexp {^stats_time} $entry]} {	             
-	    set stats_time [string trim [lindex [split $entry "="] 1]];
-	}
-    }
-
-    set seconds [clock seconds];
- 
-    # make the test
-    set code 0;
-
-    set stats_timediff [expr $seconds - $stats_time];
-    if {[expr $stats_timediff > 60]} {
-	set code $err_stats_time;
-    }
-
-    set chstats_timediff [expr $seconds - $chstats_time];
-    if {[expr $chstats_timediff > 60]} {
-	set code $err_chstats_time;
-    }
-
-    if {($total_files == 0) || ($total_bytes == 0)} {
-	set code $err_chstats_data;
-    }
-
-    set rlist [list $seconds $stats_time $chstats_time \
-		   $stats_timediff $chstats_timediff \
-		   $total_files $total_bytes];
-    set r [join $rlist " "];
-    append r "\n";
-
-    return [join [list "|monitorcode=$code" $r] "|"];
 }
