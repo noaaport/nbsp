a756c9d59cc277ee918b96e22ba170eed46c1fb7
Jose F Nieves <nieves@volkron.net>
2024-07-13 19:44:36 -0400

    Started the build/netbsd scripts

---
+++ build/netbsd/pkginfo.mk
@@ -0,0 +1,12 @@
+#
+# $Id$
+#
+
+# This file is read by the (bsd package) Makefile
+
+package_build != cat pkg-release
+package_category = misc
+package_ext = tgz
+package_display_file = pkg-display
+package_buildinfo_file = pkg-buildinfo
+
