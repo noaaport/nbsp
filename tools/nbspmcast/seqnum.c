0d5bb686e3ceb3278fd248f243fe90e033e2183b
Jose F Nieves <nieves@volkron.net>
2024-11-10 13:13:04 -0400

    Corrected the comment about the determination of the seqnum in nbspmcast in the get_seqnum() function.

--- tools/nbspmcast/seqnum.c
+++ tools/nbspmcast/seqnum.c
@@ -6,21 +6,21 @@
 
 uint32_t get_seqnum_start(void) {
   /*
-   * Returns the usecs ellapsed in the current hour. This will be used as the
+   * Returns the msecs ellapsed in the current hour. This will be used as the
    * seqnum. Of course it will reset every hour.
    */
   struct timeval tv;
   struct timezone tz;
   time_t now_secs;
   struct tm *now_tm;
-  uint32_t usecs; /* usecs since start of the hour */
+  uint32_t msecs; /* msecs since start of the hour */
     
   now_secs = time(NULL);
   now_tm = localtime(&now_secs);
   (void)gettimeofday(&tv, &tz);
 
-  usecs =  (now_tm->tm_min * 60 + now_tm->tm_sec) * 1000;
-  usecs += tv.tv_usec/1000;
+  msecs =  (now_tm->tm_min * 60 + now_tm->tm_sec) * 1000;
+  msecs += tv.tv_usec/1000;
 
   return(usecs);
 }
